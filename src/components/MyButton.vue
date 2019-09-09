2962c45448360b3bce7c31e347c861b2c964e7e7
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-09-10 07:42:18 +0900

    feat(coding): onclickリスナの追加

---
+++ src/components/MyButton.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <button @click="onClick">MyButton</button>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from "vue-property-decorator";
+
+    @Component
+    export default class MyButton extends Vue{
+        @Prop()
+        public greet?: string;
+
+        public onClick(){
+            alert(this.greet);
+        }
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+   <MyButton greet="hello!"></MyButton>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import MyButton  from "@/components/MyButton.vue";
 
 @Component({
   components: {
-    HelloWorld,
+    MyButton
   },
 })
 export default class Home extends Vue {}
